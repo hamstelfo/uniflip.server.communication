855f34c577a677b978173fe762703a9a13641352
Rubén Peinado <rpeinado@asystem.es>
2018-11-22 11:43:14 +0100

    test

--- database.php
+++ database.php
@@ -1,6 +1,12 @@
 <?
 
-function getCatalog($memberId, $catalogId, $asJson= false)
+define("STATUS_LINUX_CUTTER_0", "Starting Cutter communication");
+define("STATUS_LINUX_CUTTER_1", "Sending info to the Cutter");
+define("STATUS_LINUX_CUTTER_2", "Sending file to the Cutter");
+define("STATUS_LINUX_CUTTER_3", "");
+define("STATUS_LINUX_CUTTER_4", "");
+
+function getCatalogInfo($memberId, $catalogId, $asJson= false)
 {
 	dbOpen();
 	$sql= "SELECT * FROM `catalogs` WHERE `id` = '".$catalogId."' AND `userid`='".$memberId."' \r\n";
@@ -21,6 +27,17 @@ function getCatalog($memberId, $catalogId, $asJson= false)
 	}
 
 	dbClose();
+}
+
+
+function updateCatalogPDFInfo($memberId, $catalogId, $pdfInfo)
+{
+	dbOpen();
+	$sql= "UPDATE `catalogs` SET `pdfinfo` = '".$pdfInfo."' WHERE `id` = '".$catalogId."' AND `userid`='".$memberId."' \r\n";
+	$result = dBQuery($sql) or die("Query failed: " . dBError());
+	dbClose();
+
+	return $result;
 }
 
 ?>
